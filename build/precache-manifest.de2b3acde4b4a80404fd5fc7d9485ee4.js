390191477f1a7c9fe3ec7a218918d6bd1d32fc1e
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2020-06-07 12:14:42 +0200

    add build to git

---
+++ build/precache-manifest.de2b3acde4b4a80404fd5fc7d9485ee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef8019fe30a37e0498e06f0ac7c63f8",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "7850f82e53b0b9970856",
+    "url": "/docs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d1cfcc29daa0c7dbd22",
+    "url": "/docs/static/js/2.1e079ef0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/docs/static/js/2.1e079ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7850f82e53b0b9970856",
+    "url": "/docs/static/js/main.cf68a7d6.chunk.js"
+  },
+  {
+    "revision": "3ec08dc67e5f35f60a8d",
+    "url": "/docs/static/js/runtime-main.c697452b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/docs/static/media/logo.5d5d9eef.svg"
+  }
+]);
