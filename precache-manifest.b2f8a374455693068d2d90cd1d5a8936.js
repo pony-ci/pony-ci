c9db02ba1cefb8bd16cc6336cfd71b209efdaeb9
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2020-06-07 19:15:43 +0200

    Updates

---
+++ precache-manifest.b2f8a374455693068d2d90cd1d5a8936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca1f41669e0143b92797bae9bf50aaa",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "9449fe8b2e9437d30784",
+    "url": "/docs/static/css/main.248d2f65.chunk.css"
+  },
+  {
+    "revision": "f5a9cdbc519b881cfe11",
+    "url": "/docs/static/js/2.d8ebe34b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/docs/static/js/2.d8ebe34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9449fe8b2e9437d30784",
+    "url": "/docs/static/js/main.cc3be522.chunk.js"
+  },
+  {
+    "revision": "21ed75ccafc6af46f8fe",
+    "url": "/docs/static/js/runtime-main.f8723f90.js"
+  }
+]);
